e24d3f0d5641b9a70400fffbe437366248c95eb7
Vojislav <vojislavmladenovic1@gmail.com>
2025-12-23 16:26:17 +0100

    promenjen usersData.js file

--- src/data/usersData.js
+++ src/data/usersData.js
@@ -1,24 +1,33 @@
 const users = [
     {
-        username: 'pera',
-        password: 'pera',
         firstname: 'Petar',
         lastname: 'Petrovic',
+        phone: '+381661234567',
+        address: 'Nemanjina 1, Beograd',
+        username: 'pera',
+        password: 'pera',
+        email: 'pera@gmail.com',
         type: 0
     },
     {
+        firstname: 'Zivorad',
+        lastname: 'Zivanovic',
+        phone: '+381661111111',
+        address: 'Takovska 1, Beograd',
         username: 'zika',
         password: 'zika',
-        firstname: 'Zika',
-        lastname: 'Zikic',
+        email: 'zika@gmail.com',
         type: 0
     },
     {
+        firstname: 'Admin',
+        lastname: 'Adminovic',
+        phone: '+381669999999',
+        address: 'Terazije 1, Beograd',
         username: 'admin',
         password: 'admin',
-        firstname: 'admin',
-        lastname: 'admin',
-        type: 1
+        email: 'admin@admin.com',
+        type: 0
     }
 ]
 
